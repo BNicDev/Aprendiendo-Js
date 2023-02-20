1c9f545d5273cef304f5f89f4080250b7ebf18ab
BNicDev <121734134+BNicDev@users.noreply.github.com>
2023-02-20 19:30:35 -0300

    Add files via upload

---
+++ Practica Js/script.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1'); 
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('.parrafito');
+const pid = document.getElementById('#pid');
+const input = document.querySelector('input');
+
+console.log(input.value)//obtiene el valor del input
